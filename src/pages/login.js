880d606ccb58f86e1847727dcd603f3219c6176c
pablo1611 <pablo161198@gmail.com>
2025-01-09 23:40:55 +0200

    Fix API endpoint in login.js to use relative path for authentication request

--- src/pages/login.js
+++ src/pages/login.js
@@ -21,7 +21,7 @@ export default function Login() {
     setErrorDetails('');
 
     try {
-      const response = await fetch('/api/auth/login', {
+      const response = await fetch('./api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
